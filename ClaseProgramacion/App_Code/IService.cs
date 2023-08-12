f5807dcfb08392ce972202afe2a9460e8362f3c1
Nuila30 <119148388+Nuila30@users.noreply.github.com>
2023-08-12 17:50:13 -0600

    Clase 1
    Conexion con la base de datos
    y la creacion de servicios
    
    --Servicio Creado
    --Consumir los servicios

---
+++ ClaseProgramacion/App_Code/IService.cs
@@ -0,0 +1,14 @@
+﻿using System.Data;
+using System.ServiceModel;
+
+// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+[ServiceContract]
+public interface IService
+{
+
+    [OperationContract]
+    DataSet ValidarUsuario(string Login, string Password);
+
+    // TODO: agregue aquí sus operaciones de servicio
+}
+
---
+++ ClaseProgramacion/App_Code/Service.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Configuration;// Para el Acceso al Web.config
+using System.Data;
+using System.Data.SqlClient;// clases base para sql-server
+// NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código, en svc y en el archivo de configuración.
+public class Service : IService
+{
+    DataSet ds = new DataSet();
+    SqlDataAdapter da;
+    string Conexion = ConfigurationManager.ConnectionStrings["ConexionSql"].ConnectionString;
+    string Patron = ConfigurationManager.AppSettings["Patron"].ToString();
+
+    public DataSet ValidarUsuario(string Login, string Password)
+    {
+        try
+        {
+            da = new SqlDataAdapter("SP_ValidarUsuario", Conexion);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            da.SelectCommand.Parameters.AddWithValue("@login", Login);
+            da.SelectCommand.Parameters.AddWithValue("@Password", Password);
+            da.SelectCommand.Parameters.AddWithValue("@Patron ", Patron);
+            da.Fill(ds, "PerfilUsuario");
+
+        }
+        catch (Exception ex)
+        {
+            ds = null;
+        }
+        return ds;
+    }
+
+
+}
